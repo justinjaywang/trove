e91b608b971ad4d02aa5e855c69a72aec6bcddca
justinjaywang <justinjaywang@gmail.com>
2014-06-21 14:56:05 -0700

    Added item elements

--- source-1/js/models/item.js
+++ source-1/js/models/item.js
@@ -25,8 +25,8 @@ App.Item.FIXTURES = [{
   name: 'Zig-Zag Soap Dish',
   price: 0,
   description: 'Aviators stone roses kosovo war mad props, rollerblades simpsons trl trainers barbie. Ford explorer carpenter pants bandanas seinfeld friends ace ventura. Chillax oasis rachel haircut slap bracelet nerf guns miss cleo.',
-  imageUrl: 'http://placehold.it/600x600',
-  thumbnailUrl: 'http://placehold.it/300x300',
+  imageUrl: 'http://placehold.it/640x640',
+  thumbnailUrl: 'http://placehold.it/320x320',
   category: 'bathroom',
   tags: 'bathroom;minimal',
   creationDate: 'Mon, 26 Aug 2013 20:23:43 GMT'
@@ -35,8 +35,8 @@ App.Item.FIXTURES = [{
   name: 'Citrus Juicer',
   price: 1,
   description: 'Hulk hogan maze screensavers savage garden alanis morissette da bomb ain’t no thang german reunification, bop it videocassette baby got back once you pop you can’t stop kazaa. Gak spice girls mighty morphin power rangers maxin’ home alone. Lorem enrique iglesias glow in the dark stickers rodney king riots bill clinton britpop.',
-  imageUrl: 'http://placehold.it/600x600',
-  thumbnailUrl: 'http://placehold.it/300x300',
+  imageUrl: 'http://placehold.it/640x640',
+  thumbnailUrl: 'http://placehold.it/320x320',
   category: 'kitchen',
   tags: 'kitchen',
   creationDate: 'Fri, 07 Aug 2013 10:10:10 GMT'
@@ -45,10 +45,41 @@ App.Item.FIXTURES = [{
   name: 'Toothbrush Cap',
   price: 0,
   description: 'Fresh hey arnold playa garth brooks nelson mandela, christina aguilera boo ya vanilla ice mariah carey clueless.',
-  imageUrl: 'http://placehold.it/600x600',
-  thumbnailUrl: 'http://placehold.it/300x300',
+  imageUrl: 'http://placehold.it/640x640',
+  thumbnailUrl: 'http://placehold.it/320x320',
   category: 'bathroom',
   tags: 'bathroom',
+  creationDate: 'Fri, 07 Aug 2013 10:10:10 GMT'
+}, {
+  id: 4,
+  name: 'Toothbrush Stand',
+  price: 0,
+  description: 'Madonna lisa frank nerf guns fanny packs aol instant messenger.',
+  imageUrl: 'http://placehold.it/640x640',
+  thumbnailUrl: 'http://placehold.it/320x320',
+  category: 'bathroom',
+  tags: 'bathroom;minimal',
+  creationDate: 'Fri, 07 Aug 2013 10:10:10 GMT'
+}, {
+  id: 5,
+  name: 'Makeup Box',
+  price: 0,
+  description: 'Walkman bare midriffs boris yeltsin skip-it acid wash gulf war. Korn the matrix flip flops frasier da bomb des.',
+  imageUrl: 'http://placehold.it/640x640',
+  thumbnailUrl: 'http://placehold.it/320x320',
+  category: 'bathroom',
+  tags: 'bathroom;storage',
+  creationDate: 'Fri, 07 Aug 2013 10:10:10 GMT'
+}, {
+  id: 6,
+  name: 'Cookie Cutter Set',
+  price: 1,
+  description: 'Amet cut-off jean shorts green day generation y miss cleo, leotards pearl jam trainers phat princess diana animated gifs bop it. Armageddon overalls ford explorer home alone home improvement alternative rock highlights, environmentalism apollo 13 kurt cobain ’n sync.
+',
+  imageUrl: 'http://placehold.it/640x640',
+  thumbnailUrl: 'http://placehold.it/320x320',
+  category: 'kitchen',
+  tags: 'kitchen',
   creationDate: 'Fri, 07 Aug 2013 10:10:10 GMT'
 }
 ];
