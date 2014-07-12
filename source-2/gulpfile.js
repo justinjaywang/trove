cc0d7b8ae66a4f0fe6edd93cd3e08473cb77eb8d
justinjaywang <justinjaywang@gmail.com>
2014-07-12 14:02:28 -0700

    Added React Hello World

---
+++ source-2/gulpfile.js
@@ -0,0 +1,92 @@
+var gulp =          require('gulp');
+var clean =         require('gulp-clean');
+var watch =         require('gulp-watch');
+var react =         require('gulp-react');
+var concat =        require('gulp-concat');
+var uglify =        require('gulp-uglify');
+var less =          require('gulp-less');
+var autoprefixer =  require('gulp-autoprefixer');
+var minifycss =     require('gulp-minify-css');
+var rename =        require('gulp-rename');
+var imagemin =      require('gulp-imagemin');
+var cache =         require('gulp-cache');
+var changed =       require('gulp-changed');
+var util =          require('gulp-util');
+
+var paths = {
+  templates: ['source/js/templates/**/*.jsx'],
+  scripts:   ['source/js/**/*.js'],
+  styles:    ['source/less/all.less'],
+  images:    ['source/img/**/*']
+};
+
+var dest = {
+  build:  'build'
+};
+dest.scripts =    dest.build + '/js';
+dest.styles =     dest.build + '/css';
+dest.images =     dest.build + '/img';
+dest.templates =  dest.scripts;
+
+gulp.task('clean', function () {  
+  return gulp.src(dest.build, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('templates', function() {
+  return gulp.src(paths.templates)
+    // .pipe(handlebars({outputType: 'browser'}))
+    // .pipe(concat('templates.js'))
+    .pipe(concat('templates.js'))
+    .pipe(react())
+    .pipe(gulp.dest(dest.templates))
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest(dest.templates))
+    .on('error', util.log);
+});
+  
+// Minify and copy all JavaScript (except vendor scripts)
+gulp.task('scripts', function() {
+  return gulp.src(paths.scripts)
+    // .pipe(coffee())
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest(dest.scripts))
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest(dest.scripts))
+    .on('error', util.log);
+});
+  
+// Compile Less, autoprefix, and minify the resulting CSS
+gulp.task('styles', function () {
+  return gulp.src(paths.styles)
+    .pipe(changed(dest.styles)) // exclude unmodified files
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(dest.styles))
+    .pipe(minifycss())
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest(dest.styles))
+    .on('error', util.log);
+});
+  
+// Copy all static images
+gulp.task('images', function() {
+ return gulp.src(paths.images)
+    // Pass in options to the task
+    .pipe(cache(imagemin())) // only compress changed images
+    .pipe(gulp.dest(dest.images))
+    .on('error', util.log);
+});
+  
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.templates, ['templates']);
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(['less/**/*.less'], ['styles']);
+  gulp.watch(paths.images, ['images']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['templates', 'scripts', 'styles', 'images', 'watch']);
